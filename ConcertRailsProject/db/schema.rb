beb9870181fe70222ecca2b75f6257c95dfafee5
github username <github email address>
2020-01-24 12:52:31 -0500

    css done

--- ConcertRailsProject/db/schema.rb
+++ ConcertRailsProject/db/schema.rb
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 20200123172750) do
   create_table "venues", force: :cascade do |t|
     t.string   "name"
     t.integer  "capacity"
+    t.string   "location"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "location_id"
--- ConcertRailsProject/db/seeds.rb
+++ ConcertRailsProject/db/seeds.rb
@@ -1,40 +1,52 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Location.destroy_all
+Artist.destroy_all
+Venue.destroy_all
+Performance.destroy_all
+Concert.destroy_all
+
+
+#locations
+
 nyc = Location.create(name: 'New York City')
 dc = Location.create(name: 'Washington D.C.')
+
+#artists
+
 tool = Artist.create(genre: 'Rock', name: 'Tool', bio: 'Is a band')
-capital1 = Venue.create(name: 'Capital One', capacity: 100000, location_id: 2)
-toolconcert = Concert.create([{
+beatles = Artist.create(genre: 'Rock', name: 'The Beatles', bio: 'Is a Liverpool band')
+
+#venues
+
+msg = Venue.create(name: 'Madison Square Garden', capacity: 10, location_id: nyc.id)
+capital1 = Venue.create(name: 'Capital One', capacity: 100000, location_id: dc.id)
+
+#concerts
+
+toolconcert = Concert.create({
     :name      => "Tool Tour 2020",
-    :venue_id  => 1,
+    :venue_id  => msg.id,
     :date    => DateTime.new(2020,9,1,19)
-  }])
+  })
 
-beatles = Artist.create(genre: 'Rock', name: 'The Beatles', bio: 'Is a british band')
-
-toolatcapital1 = Performance.create(artist_id: 1, concert_id: 1, headliner: true)
-beatlesatcapital1 = Performance.create(artist_id: 2, concert_id: 1, headliner: false)
-
-
-
-msg = Venue.create(name: 'Madison Square Garden', capacity: 10, location_id: 1)
-beatlesconcert = Concert.create([{
+  beatlesconcert = Concert.create({
     :name      => "Beatle Reunion 2020",
-    :venue_id  => 2,
+    :venue_id  => capital1.id,
     :date    => DateTime.new(2020,10,1,19)
-    }])
-    
-    beatlesatmsg = Performance.create(artist_id: 2, concert_id: 2, headliner: true)
-    beatlesconcert2 = Concert.create([{
+    })
+
+  beatlesconcert2 = Concert.create({
       :name      => "Beatle Without Ringo 2020",
-      :venue_id  => 1,
+      :venue_id  => msg.id,
       :date    => DateTime.new(2019,10,1,19)
-      }])
-      
-      beatlesatc1 = Performance.create(artist_id: 2, concert_id: 3, headliner: true)
-      
+      })
+
+#Performances
+
+toolatcapital1 = Performance.create(artist_id: tool.id, concert_id: toolconcert.id, headliner: true)
+beatlesatcapital1 = Performance.create(artist_id: beatles.id, concert_id: beatlesconcert.id, headliner: false)
+beatlesatmsg = Performance.create(artist_id: beatles.id, concert_id: beatlesconcert2.id, headliner: true)
+beatlesatc1 = Performance.create(artist_id: beatles.id, concert_id: beatlesconcert.id, headliner: true)
+
+
+# byebug
+# 0
